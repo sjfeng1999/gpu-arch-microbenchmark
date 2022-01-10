96752c38682b72151a929ebd9788351509748eb8
willer <42251744+sjfeng1999@users.noreply.github.com>
2022-01-10 11:10:25 +0800

    update shared-memory load kernel

--- memory/op_ld_shared.cu
+++ memory/op_ld_shared.cu
@@ -1,28 +1,59 @@
-// kernel compare with bank-conflict and without
-
-
 __global__ void ld_shared_without_conflict(float *A, float *B, uint32_t *cost){
     int tid = threadIdx.x;
-
+    uint32_t laneid = get_laneid();
+    uint32_t start, stop;
     extern __shared__ shArray[];
 
     float dummy = 0;
     float vA, vB, vC, vD;
-    float *ptr;
-    ptrdiff_t offset = 0;
+    ptrdiff_t offset = laneid;
+    float *ptr = shArray + offset;
 
+    start = get_clock();
     asm volatile(
         "ld.shared.ca.f32   %0,     [%4];       \n\t"
-        "ld.shared.ca.f32   %1,     [%4+4];     \n\t"
-        "ld.shared.ca.f32   %2,     [%4+8];     \n\t"
-        "ld.shared.ca.f32   %3,     [%4+12];    \n\t"
-        :"=f"(vA[0]),"=f"(vB[0]),"=f"(vC[0]),"=f"(vD[0])
+        "ld.shared.ca.f32   %1,     [%4+128];   \n\t"
+        "ld.shared.ca.f32   %2,     [%4+256];   \n\t"
+        "ld.shared.ca.f32   %3,     [%4+384];   \n\t"
+        :"=f"(vA),"=f"(vB),"=f"(vC),"=f"(vD)
         :"l"(ptr):"memory"
     );
-    dummy += vA[0];
-    dummy += vB[0];
-    dummy += vC[0];
-    dummy += vD[0];
+    bar_sync();
+    stop = get_clock();
 
+    cost[tid] = stop - start;
+    dummy += vA;
+    dummy += vB;
+    dummy += vC;
+    dummy += vD;
+    B[tid] = dummy;
+}
+
+
+__global__ void ld_shared_vec_with_conflict(float *A, float *B, uint32_t *cost){
+    int tid = threadIdx.x;
+    uint32_t laneid = get_laneid();
+    uint32_t start, stop;
+    extern __shared__ shArray[];
+
+    float dummy = 0;
+    float vA, vB, vC, vD;
+    ptrdiff_t offset = 4 * laneid;
+    float *ptr = shArray + offset;
+
+    start = get_clock();
+    asm volatile(
+        "ld.shared.ca.v4.f32   {%0, %1, %2, %3},     [%4]; \n\t"
+        :"=f"(vA),"=f"(vB),"=f"(vC),"=f"(vD)
+        :"l"(ptr):"memory"
+    );
+    bar_sync();
+    stop = get_clock();
+
+    cost[tid] = stop - start;
+    dummy += vA;
+    dummy += vB;
+    dummy += vC;
+    dummy += vD;
     B[tid] = dummy;
 }
